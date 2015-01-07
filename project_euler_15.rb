81bc657ff8898172d7a77096ecf381a1facf6e2f
Rustam Gasanov <rustamagasanov@gmail.com>
2015-01-07 23:12:33 +0700

    [+] added pe15 solution

---
+++ project_euler_15.rb
@@ -0,0 +1,44 @@
+require 'pp'
+
+class LathicePaths
+  attr_reader :paths_matrix
+
+  def initialize(size)
+    @paths_matrix = Array.new(size + 1) { Array.new(size + 1).fill(0) }
+  end
+
+  def calculate
+    0.upto(paths_matrix_size - 1).each do |layer|
+      fill_layer(layer)
+    end
+  end
+
+  def output
+    # pp paths_matrix
+    pp paths_matrix[paths_matrix_size - 1][paths_matrix_size - 1]
+  end
+
+  private
+  def fill_layer(layer)
+    (layer..(paths_matrix_size - 1)).each do |i|
+      (layer..(paths_matrix_size - 1)).each do |j|
+        if i == layer || j == layer
+          if layer == 0
+            @paths_matrix[i][j] = 1
+          else
+            @paths_matrix[i][j] = paths_matrix[i][j - 1] + paths_matrix[i - 1][j]
+          end
+        end
+      end
+    end
+  end
+
+  def paths_matrix_size
+    @paths_matrix_size ||= paths_matrix.size
+  end
+end
+
+o = LathicePaths.new(20)
+o.calculate
+o.output
+
