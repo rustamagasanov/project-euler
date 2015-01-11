d2904043532fa2f5c9009bb544b3ea6d0e09837b
Rustam Gasanov <rustamagasanov@gmail.com>
2015-01-11 20:32:05 +0700

    [+] added pe7 description

--- project_euler_7.rb
+++ project_euler_7.rb
@@ -1,3 +1,8 @@
+# 10001st prime
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
+
 def prime?(num)
   return false if num == 0 || num == 1
   (2..Math.sqrt(num).to_i).each do |i|
