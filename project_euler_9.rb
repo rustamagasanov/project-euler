7fed682c08cdfa12a3c61a2b6dc1034ee31d8b52
Rustam Gasanov <rustamagasanov@gmail.com>
2015-01-14 08:52:59 +0700

    [^] updated pe9

--- project_euler_9.rb
+++ project_euler_9.rb
@@ -6,15 +6,10 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-def pithagorean_triplet?(a, b, c)
-  a*a + b*b == c*c
-end
-
 combinations = (1..500).to_a.repeated_combination(3).to_a
 combinations_index = 1
-complete_flag = false
 
-while !complete_flag
+loop do
   correct_triplet = false
 
   while !correct_triplet
@@ -23,8 +18,8 @@ while !complete_flag
     correct_triplet = true if a < b && b < c
   end
 
-  if pithagorean_triplet?(a, b, c)
+  if a + b + c == 1000 && a*a + b*b == c*c
     puts "Triplet found: #{ a }^2(#{ a**2 }) + #{ b }^2(#{ b**2 }) = #{ c }^2(#{ c**2 }); a + b + c = #{ a + b + c }; a * b * c = #{ a * b * c }"
-    complete_flag = true if a + b + c == 1000
+    break
   end
 end
