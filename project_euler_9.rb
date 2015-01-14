f5a9193625aa17d5a3f6b0b0db5a39394d04bb9e
Rustam Gasanov <rustamagasanov@gmail.com>
2015-01-14 09:13:03 +0700

    [^] simplified pe9 to one-liner

--- project_euler_9.rb
+++ project_euler_9.rb
@@ -6,20 +6,4 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-combinations = (1..500).to_a.repeated_combination(3).to_a
-combinations_index = 1
-
-loop do
-  correct_triplet = false
-
-  while !correct_triplet
-    a, b, c = combinations[combinations_index]
-    combinations_index += 1
-    correct_triplet = true if a < b && b < c
-  end
-
-  if a + b + c == 1000 && a*a + b*b == c*c
-    puts "Triplet found: #{ a }^2(#{ a**2 }) + #{ b }^2(#{ b**2 }) = #{ c }^2(#{ c**2 }); a + b + c = #{ a + b + c }; a * b * c = #{ a * b * c }"
-    break
-  end
-end
+(1..500).to_a.repeated_combination(3) { |c| puts c[0]*c[1]*c[2] if c[0] + c[1] + c[2] == 1000 && c[0] < c[1] && c[1] < c[2] && c[0]*c[0] + c[1]*c[1] == c[2]*c[2] }
