853f94336f343b169e1091c4ed5a4fa8253ab80c
Rustam Gasanov <rustamagasanov@gmail.com>
2015-01-29 20:07:21 +0200

    [-] removed unnecessary longest array storage

--- project_euler_14.rb
+++ project_euler_14.rb
@@ -23,14 +23,12 @@ def collatz_sequence(n, chain = [n])
 end
 
 longest_chain_length = 0
-longest_chain = []
 longest_chain_starting_number = 0
 (1..1_000_000).each do |n|
   chain = collatz_sequence(n)
   chain_length = chain.length
   if chain_length > longest_chain_length
     longest_chain_starting_number = n
-    longest_chain = chain
     longest_chain_length = chain_length
   end
 end
