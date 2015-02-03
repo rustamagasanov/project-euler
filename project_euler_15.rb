9f6de0cec821c340be35cd41a465a69a336bc433
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-03 21:46:50 +0700

    [^] simplified pe15

--- project_euler_15.rb
+++ project_euler_15.rb
@@ -2,8 +2,6 @@
 #Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 #
 #How many such routes are there through a 20×20 grid?
-require 'pp'
-
 class LathicePaths
   attr_reader :paths_matrix
 
@@ -12,14 +10,7 @@ class LathicePaths
   end
 
   def calculate
-    0.upto(paths_matrix_size - 1).each do |layer|
-      fill_layer(layer)
-    end
-  end
-
-  def output
-    # pp paths_matrix
-    pp paths_matrix[paths_matrix_size - 1][paths_matrix_size - 1]
+    0.upto(paths_matrix_size - 1).each { |layer| fill_layer(layer) }
   end
 
   private
@@ -27,11 +18,7 @@ class LathicePaths
     (layer..(paths_matrix_size - 1)).each do |i|
       (layer..(paths_matrix_size - 1)).each do |j|
         if i == layer || j == layer
-          if layer == 0
-            @paths_matrix[i][j] = 1
-          else
-            @paths_matrix[i][j] = paths_matrix[i][j - 1] + paths_matrix[i - 1][j]
-          end
+         @paths_matrix[i][j] = (layer == 0) ? 1 : paths_matrix[i][j - 1] + paths_matrix[i - 1][j]
         end
       end
     end
@@ -44,5 +31,5 @@ end
 
 o = LathicePaths.new(20)
 o.calculate
-o.output
+puts o.paths_matrix[20][20]
 
