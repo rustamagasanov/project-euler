92fc4bfa1fa3dc8d4fbf2832f289b632493b4dd4
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-07 23:25:03 +0700

    [^] simplified pe17

--- project_euler_17.rb
+++ project_euler_17.rb
@@ -6,43 +6,32 @@
 #
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
-class NumberLetters
-  attr_reader :number
+module NumberToWords
+  extend self
 
-  def initialize(number)
-    @number = number
-  end
-
-  def result
-    convert(number)
-  end
-
-  def output
-    puts convert(number)
-  end
-
-  private
   def convert(num)
     number_chars = num.to_s.chars.to_a
-    if num < 10
+    case
+    when num < 10
       one_to_nine[num - 1]
-    elsif num < 100 && num % 10 == 0
+    when num < 100 && num % 10 == 0
       decades[number_chars[0].to_i - 1]
-    elsif num < 20
+    when num < 20
       eleven_to_nineteen[number_chars[1].to_i - 1]
-    elsif num < 100
+    when num < 100
       "#{ decades[number_chars[0].to_i - 1] } #{ one_to_nine[number_chars[1].to_i - 1] }"
-    elsif num < 1000 && num % 100 == 0
+    when num < 1000 && num % 100 == 0
       "#{ one_to_nine[number_chars[0].to_i - 1] } hundred"
-    elsif num < 1000
+    when num < 1000
       "#{ one_to_nine[number_chars[0].to_i - 1] } hundred and #{ convert(number_chars[1..2].join.to_i) }"
-    elsif num == 1000
-      "one thousand"
+    when num == 1000
+      'one thousand'
     else
-      ""
+      ''
     end
   end
 
+  private
   def one_to_nine
     @one_to_nine ||= %w[one two three four five six seven eight nine]
   end
@@ -56,9 +45,4 @@ class NumberLetters
   end
 end
 
-string = ""
-(1..1000).each do |n|
-  string << NumberLetters.new(n).result
-end
-puts string.gsub(' ', '').length
-
+puts (1..1000).inject('') { |memo, i| memo += NumberToWords.convert(i) }.gsub(' ', '').length
