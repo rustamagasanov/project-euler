0ae5dfb9ddfcda853d1f033189fb8bf8e2d30cc7
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-09 21:03:52 +0200

    [^] refactored pe22

--- project_euler_22.rb
+++ project_euler_22.rb
@@ -2,11 +2,9 @@ names = File.read('project_euler_22.txt').gsub('"', '').split(',').sort
 
 alphabet = ('A'..'Z').to_a
 
-i = 0
-puts names.inject(0) { |total_score, name|
-  chars_score = name.each_char.inject(0) do |char_score, char|
+puts names.each_with_index.inject(0) { |total_score, pair|
+  name, index = pair
+  total_score + name.each_char.inject(0) do |char_score, char|
     char_score + alphabet.index(char) + 1
-  end
-  i += 1
-  total_score + chars_score * i
+  end * index
 }
