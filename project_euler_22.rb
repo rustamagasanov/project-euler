57fdfbdb12748cf469fa56eb8b40d24f89951a29
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-09 21:00:14 +0200

    [^] speed up pe22 x10 times

--- project_euler_22.rb
+++ project_euler_22.rb
@@ -2,9 +2,11 @@ names = File.read('project_euler_22.txt').gsub('"', '').split(',').sort
 
 alphabet = ('A'..'Z').to_a
 
+i = 0
 puts names.inject(0) { |total_score, name|
   chars_score = name.each_char.inject(0) do |char_score, char|
     char_score + alphabet.index(char) + 1
   end
-  total_score + chars_score * (names.index(name) + 1)
+  i += 1
+  total_score + chars_score * i
 }
