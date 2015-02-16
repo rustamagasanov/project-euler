b356e2679bfb6c4bd73b781481edfa026577e983
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-16 14:04:03 +0700

    [^] simplified factorial calculation in pe20

--- project_euler_20.rb
+++ project_euler_20.rb
@@ -7,9 +7,4 @@
 #
 # Find the sum of the digits in the number 100!
 
-def factorial(n, product = 1)
-  return 0 if n < 1
-  (n != 1) ? factorial(n - 1, product * n) : product
-end
-
-puts factorial(100).to_s.chars.map(&:to_i).reduce(:+)
+puts (1..100).inject(1, :*).to_s.chars.map(&:to_i).reduce(:+)
