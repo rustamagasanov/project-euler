745633fc4a0cbdba5221109d0789777170946af3
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-18 21:56:51 +0200

    [+] added explanation for pe34

--- project_euler_34.rb
+++ project_euler_34.rb
@@ -1,4 +1,8 @@
-puts (10..100_000).inject { |sum, number|
+# 9! * 6 = 2_177_280 : maximum sum of factorials for 6-digit number = 7-digit number,
+# thus it is possible to find an answer in range 1..999_999
+# 9! * 7 = 2_540_160 : maximum sum of factorials for 7-digit number = 7-digit number,
+# thus correct upper bound is 2_540_160
+puts (10..2_540_160).inject { |sum, number|
   if number == number.to_s.chars.map(&:to_i).inject(0) { |memo, n| memo + (1..n).inject(1, :*) }
     sum + number
   else
