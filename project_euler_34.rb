2844e6e7916b7551733b196ca30e4d585f83ae40
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-18 21:58:24 +0200

    [+] added pe34 description

--- project_euler_34.rb
+++ project_euler_34.rb
@@ -1,3 +1,11 @@
+# Digit factorials
+# Problem 34
+# 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+#
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+#
+# Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
 # 9! * 6 = 2_177_280 : maximum sum of factorials for 6-digit number = 7-digit number,
 # thus it is possible to find an answer in range 1..999_999
 # 9! * 7 = 2_540_160 : maximum sum of factorials for 7-digit number = 7-digit number,
