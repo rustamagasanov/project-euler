9d92c35edf2122b73d1d3941db0da0b50349fefb
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-22 15:47:48 +0700

    [+] added pe36 description

--- project_euler_36.rb
+++ project_euler_36.rb
@@ -1,3 +1,11 @@
+# Double-base palindromes
+# Problem 36
+# The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+#
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+#
+# (Please note that the palindromic number, in either base, may not include leading zeros.)
+
 puts (1..999_999).select { |num|
   num.to_s == num.to_s.reverse && num.to_s(2) == num.to_s(2).reverse
 }.reduce(:+)
