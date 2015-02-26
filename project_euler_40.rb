a5c9acd667a9885518d8bd6271fab5846fe8aaba
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-26 21:31:17 +0700

    [+] added pe40 description

--- project_euler_40.rb
+++ project_euler_40.rb
@@ -1,3 +1,15 @@
+# Champernowne's constant
+# Problem 40
+# An irrational decimal fraction is created by concatenating the positive integers:
+#
+# 0.123456789101112131415161718192021...
+#
+# It can be seen that the 12th digit of the fractional part is 1.
+#
+# If dn represents the nth digit of the fractional part, find the value of the following expression.
+#
+# d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
 c = '0'
 loop.each_with_index do |_, i|
   c << (i + 1).to_s
