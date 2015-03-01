1ecaef0d4b9624123a7a61c28a66a086554a99c5
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-01 23:31:43 +0700

    [^] added pe48 description

--- project_euler_48.rb
+++ project_euler_48.rb
@@ -1,2 +1,7 @@
+# Self powers
+# Problem 48
+# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+#
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 puts (1..1000).inject(0) { |memo, i| memo += i**i; memo }.to_s[-10..-1]
 
