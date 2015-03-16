34b989c86b3588afd6ca56a2e54ad6d6e87fa4a6
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-16 20:31:20 +0300

    [+] added pe63

--- project_euler_63.rb
+++ project_euler_63.rb
@@ -1,3 +1,9 @@
+# Powerful digit counts
+# Problem 63
+# The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
+#
+# How many n-digit positive integers exist which are also an nth power?
+
 # 10^n will never have n digits so 1..9
 count = 0
 1.upto(9) do |i|
