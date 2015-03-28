bb138c34438ed816ca54b208acf43949e3d988b6
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-28 18:47:10 +0300

    [^] math approach for pe44

--- project_euler_44.rb
+++ project_euler_44.rb
@@ -1,23 +1,24 @@
 pentagon_nums = []
-sums  = []
-pairs = []
 
-loop.with_index(1) do |_, n|
-  current_pentagon_num =  n * (3 * n - 1) / 2
-  pentagon_nums << current_pentagon_num
+catch(:done) do
+  loop.with_index(1) do |_, n|
+    current_pentagon_num =  n * (3 * n - 1) / 2
+    pentagon_nums << current_pentagon_num
 
-  if sums.detect { |sum| sum == current_pentagon_num }
-    index = sums.find_index(current_pentagon_num)
-    puts pairs[index][0] - pairs[index][1]
-    break
-  end
+    pentagon_nums.each_with_index do |pentagon_num, i|
+      break if i == pentagon_nums.size - 1
+      if pentagon_nums.include?(pentagon_nums.last - pentagon_num)
+        d = 1 + 24 * (pentagon_nums.last + pentagon_num)
+        if d > 0
+          solution_1 = (1 + Math.sqrt(d)) / 6
+          solution_2 = (1 - Math.sqrt(d)) / 6
 
-  pentagon_nums.each_with_index do |pentagon_num, i|
-    break if i == pentagon_nums.size - 1
-
-    if pentagon_nums.include?(pentagon_nums.last - pentagon_num)
-      sums  << pentagon_nums.last + pentagon_num
-      pairs << [pentagon_nums.last, pentagon_num]
+          unless [solution_1, solution_2].select { |s| s > 0 && s % 1 == 0 }.first.nil?
+            puts pentagon_nums.last - pentagon_num
+            throw(:done)
+          end
+        end
+      end
     end
   end
 end
