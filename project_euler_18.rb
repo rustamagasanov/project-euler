c82fc0d7ff6bee20a566e180c14be7bf2d67aa6b
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-31 23:03:47 +0300

    [+] added pe18 description

--- project_euler_18.rb
+++ project_euler_18.rb
@@ -1,3 +1,14 @@
+# Maximum path sum I
+# Problem 18
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+# That is, 3 + 7 + 4 + 9 = 23.
+#
+# Find the maximum total from top to bottom of the triangle below:
+
 module InputMatrix
   def self.content
     [ %w[75],
