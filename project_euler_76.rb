88744a30e0a864d553ca37cdc5adfb0754884c85
Rustam Gasanov <rustamagasanov@gmail.com>
2015-04-14 20:59:52 +0300

    [+] added pe76 solution

--- project_euler_76.rb
+++ project_euler_76.rb
@@ -1,13 +1,25 @@
-class Summation
-  def self.calculate(n, res = [])
-    n.downto(1).each do |i|
-      next if i == n
-      if i == 1
-        p res
-      end
-      calculate(n - i, res << n)
-    end
+# Counting summations
+# Problem 76
+# It is possible to write five as a sum in exactly six different ways:
+#
+# 4 + 1
+# 3 + 2
+# 3 + 1 + 1
+# 2 + 2 + 1
+# 2 + 1 + 1 + 1
+# 1 + 1 + 1 + 1 + 1
+#
+# How many different ways can one hundred be written as a sum of at least two positive integers?
+
+target = 100
+ways = [1] + [0] * target
+
+(1...target).each do |n|
+  (n...target + 1).each do |i|
+    ways[i] += ways[i - n]
   end
 end
+# 1..4 -> n
+#
 
-Summation.calculate(5)
+puts ways[target]
