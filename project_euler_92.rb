bc6c88c653b1a3c01ab99217bc988f02988a3ac0
Rustam Gasanov <rustamagasanov@gmail.com>
2015-04-19 22:26:56 +0300

    [^] speedup pe92 x3

--- project_euler_92.rb
+++ project_euler_92.rb
@@ -1,16 +1,18 @@
 known_chains = []
 puts (2...10_000_000).inject(0) { |memo, i|
-  next_num = i
+  next_num, curr_chain_elems = i, [i]
   loop do
     digits = next_num.to_s.chars.map(&:to_i)
     next_num = digits.map { |d| d**2 }.reduce(:+)
 
+    curr_chain_elems << next_num
+
     if next_num == 1 || next_num == 89 || known_chains[next_num] != nil
       if next_num == 89 || known_chains[next_num] == 89
         memo += 1
-        known_chains[i] = 89
+        curr_chain_elems.each { |elem| known_chains[elem] = 89 }
       else
-        known_chains[i] = 1
+        curr_chain_elems.each { |elem| known_chains[elem] = 1 }
       end
       break
     end
