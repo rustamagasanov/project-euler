a3e1846ca79070e349cac236e305d0c3493d6c61
Rustam Gasanov <rustamagasanov@gmail.com>
2015-05-03 16:26:08 +0300

    [^] applied rule for even n's

--- project_euler_69.rb
+++ project_euler_69.rb
@@ -2,12 +2,24 @@ require 'benchmark'
 
 class EulersTotient
   def phi(n)
-    phi = if prime?(n)
-            n - 1
-          else
-            (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
-          end
+    # if p is prime, phi(p) = p - 1 (for p < 10^20)
+    phi = if phis[n].nil?
+      if prime?(n)
+        n - 1
+      else
+        (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
+      end
+    else
+      phis[n]
+    end
+
     phis[n] = phi
+    # if n is even, phi(2n) = 2 phi(n)
+    if n.even?
+      phis[2 * n] = 2 * phi
+    end
+
+    phi
   end
 
   def phis
@@ -27,7 +39,7 @@ t = EulersTotient.new
 
 Benchmark.bm do |x|
   x.report do
-    puts (2..1_000_000).inject(0) { |memo, n|
+    puts (2..10_000).inject(0) { |memo, n|
       p n
       res = n / t.phi(n)
       res > memo ? res : memo
