77cee8aadce0b9d1a48632ef977f614dbf13b463
Rustam Gasanov <rustamagasanov@gmail.com>
2015-05-03 16:18:32 +0300

    [^] added prime check

--- project_euler_69.rb
+++ project_euler_69.rb
@@ -1,17 +1,39 @@
+require 'benchmark'
+
 class EulersTotient
   def phi(n)
-    phi = (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
+    phi = if prime?(n)
+            n - 1
+          else
+            (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
+          end
     phis[n] = phi
   end
 
   def phis
     @phis ||= []
   end
+
+  def prime?(n)
+    return false if n < 2
+    (2..Math.sqrt(n).to_i).each do |i|
+      return false if n % i == 0
+    end
+    true
+  end
 end
 
 t = EulersTotient.new
-puts (2..1_000_000).inject(0) { |memo, n|
-  p n
-  res = n / t.phi(n)
-  res > memo ? res : memo
-}
+
+Benchmark.bm do |x|
+  x.report do
+    puts (2..1_000_000).inject(0) { |memo, n|
+      p n
+      res = n / t.phi(n)
+      res > memo ? res : memo
+    }
+  end
+
+  x.report do
+  end
+end
