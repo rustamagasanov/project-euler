89ec0e1faa93fbfe1dab47c4ae0949b51508a6db
Rustam Gasanov <rustamagasanov@gmail.com>
2015-05-22 23:54:15 +0300

    [^] upgraded pe69

--- project_euler_69.rb
+++ project_euler_69.rb
@@ -2,15 +2,33 @@ require 'benchmark'
 
 class EulersTotient
   def phi(n)
-    # if p is prime, phi(p) = p - 1 (for p < 10^20)
-    phi = if phis[n].nil?
-      if prime?(n)
-        n - 1
+
+    phi = nil
+
+    if phis[n].nil?
+      prime = true
+
+      (2..Math.sqrt(n).to_i).each do |i|
+        if n % i == 0
+          prime = false
+          # if gcd(m, n) = 1, then phi(m*n) = phi(m) * phi(n)
+          if (n / i).gcd(i) == 1
+            if !phis[n / i].nil? && !phis[i].nil?
+              phi = phis[n / i] * phis[i]
+            end
+            break
+          end
+        end
+      end
+
+      # if p is prime, phi(p) = p - 1 (for p < 10^20)
+      if phi.nil? && prime
+        phi = n - 1
       else
-        (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
+        phi = (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
       end
     else
-      phis[n]
+      phi = phis[n]
     end
 
     phis[n] = phi
@@ -39,10 +57,22 @@ t = EulersTotient.new
 
 Benchmark.bm do |x|
   x.report do
+    # puts (2..1_000_000).inject(0) { |memo, n|
+    #   p n
+    #   res = n / t.phi(n)
+    #   res > memo ? res : memo
+    # }
+
     puts (2..10_000).inject(0) { |memo, n|
-      p n
       res = n / t.phi(n)
-      res > memo ? res : memo
+      # p "#{n} -> #{res}"
+      # res > memo ? res : memo
+      if res > memo
+        p "#{n} -> f(n)=#{t.phi(n)}, #{res}"
+        res
+      else
+        memo
+      end
     }
   end
 
