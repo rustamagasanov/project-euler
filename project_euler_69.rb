8bdc4a9577a9172e8434eb84bee2d38bdda58956
Rustam Gasanov <rustamagasanov@gmail.com>
2015-05-25 22:31:28 +0300

    [^] speedup totient func

--- project_euler_69.rb
+++ project_euler_69.rb
@@ -27,102 +27,75 @@ class EulersTotientMax
   end
 
   def n_divided_phi
-    primes = EratosthenesSieve.new(limit).get_primes
     phis = Array.new(limit)
-
-    # if p is prime, phi(p) = p - 1
-    primes.each { |p| phis[p] = p - 1 }
-
-    # if n=p1*p2 phi(n) = phi(p1) * phi(p2) = (p1 - 1) * (p2 - 1)
-    (0..primes.size - 1).each do |p1|
-      ((p1 + 1)..primes.size - 1).each do |p2|
-        n = primes[p1] * primes[p2]
-        next if n > limit
-        phis[n] = (primes[p1] - 1) * (primes[p2] - 1)
+    primes = nil
+    Benchmark.bm do |x|
+      x.report do
+        puts "Getting primes"
+        primes = EratosthenesSieve.new(limit).get_primes
       end
-    end
 
-    # if n is even, phi(2n) = 2 * phi(n)
-    (2..limit).each do |n|
-      if n.even? && !phis[n].nil?
-        if 2 * n <= limit && phis[2 * n].nil?
-          phis[2 * n] = 2 * phis[n]
+
+      x.report do
+        puts "Setting p - 1"
+        # if p is prime, phi(p) = p - 1
+        primes.each { |p| phis[p] = p - 1 }
+      end
+
+      x.report do
+        puts "Setting p ^"
+        # if p is prime, phi(p^a) = p^a * (1 - 1/p)
+        primes.each do |p|
+          loop.with_index(2) do |_, power|
+            if p**power < limit
+              phis[p**power] = p**power * (1 - 1 / p)
+            else
+              break
+            end
+          end
+        end
+      end
+
+      x.report do
+        puts "Setting p1 - 1 * p2 - 1"
+        # if n=p1*p2 phi(n) = phi(p1) * phi(p2) = (p1 - 1) * (p2 - 1)
+        (0..primes.size - 1).each do |p1|
+          ((p1 + 1)..primes.size - 1).each do |p2|
+            n = primes[p1] * primes[p2]
+            next if n > limit
+            phis[n] = (primes[p1] - 1) * (primes[p2] - 1)
+          end
+        end
+      end
+
+      x.report do
+        puts "Setting even"
+        # if n is even, phi(2n) = 2 * phi(n)
+        (2..limit).each do |n|
+          if n.even? && !phis[n].nil?
+            if 2 * n <= limit && phis[2 * n].nil?
+              phis[2 * n] = 2 * phis[n]
+            end
+          end
+        end
+      end
+
+      puts phis.select { |p| p.nil? }.count
+
+      x.report do
+        puts "Other"
+        (2..limit).each do |n|
+          if phis[n].nil?
+            phis[n] = (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
+          end
         end
       end
     end
-
-    p phis
-
-    (2..limit).each do |n|
-    end
-
-    # puts (2..10).inject(0) { |memo, n|
-    #   res = n / t.phi(n)
-    #   p n if n % 1000 == 0
-    #   # p "#{n} -> #{res}"
-    #   # p "#{n} -> f(n)=#{t.phi(n)}, #{res}"
-    #   # res > memo ? res : memo
-    #   if res > memo
-    #     # p "#{n} -> f(n)=#{t.phi(n)}, #{res}"
-    #     n
-    #   else
-    #     memo
-    #   end
-    # }
-
-  end
-
-  def phi(n)
-    if phis[n].nil?
-      # if p is prime, phi(p) = p - 1 (for p < 10^20)
-      if prime?(n)
-        phi = n - 1
-        add_prime(n)
-      else
-        phi = (1..n).inject(0) { |p, i| p += 1 if n.gcd(i) == 1; p }
-      end
-    else
-      phi = phis[n]
-    end
-
-    phis[n] = phi
-    # if n is even, phi(2n) = 2 phi(n)
-    if n.even?
-      phis[2 * n] = 2 * phi
-    end
-
-    phi
-  end
-
-  def phis
-    @phis ||= []
-  end
-
-  def primes
-    @primes ||= []
-  end
-
-  # if n=p1*p2 phi(n) = phi(p1) * phi(p2) = (p1 - 1) * (p2 - 1)
-  def add_prime(p2)
-    primes.each do |p1|
-      phis[p1 * p2] = (p1 - 1) * (p2 - 1)
-    end
-    primes << p2
-  end
-
-  def prime?(n)
-    return false if n < 2
-    (2..Math.sqrt(n).to_i).each do |i|
-      return false if n % i == 0
-    end
-    true
+    res = (2..limit).map { |n| n / phis[n].to_f }
+    res.index(res.max) + 2
   end
 end
 
 
-Benchmark.bm do |x|
-  x.report do
-    # p EratosthenesSieve.new(100).get_primes
-    EulersTotientMax.new(10).n_divided_phi
-  end
-end
+# p EratosthenesSieve.new(100).get_primes
+p EulersTotientMax.new(1_0000).n_divided_phi
