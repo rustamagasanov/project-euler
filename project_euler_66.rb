67224ec5c7619df5a1e5e5da991902a9886ec3f7
Rustam Gasanov <rustamagasanov@gmail.com>
2015-06-12 14:35:11 +0300

    [^] eliminate bruteforce and unnecessary throw/catchg

--- project_euler_66.rb
+++ project_euler_66.rb
@@ -1,30 +1,14 @@
 largest_x, largest_d = 0, 0
 (2..1_000).each do |d|
   next if Math.sqrt(d) % 1 == 0.0
-  catch(:solution_found) do
-    loop.with_index(2) do |_, x|
-      y = Math.sqrt( (x**2 - 1) / d.to_f )
-      if y % 1 == 0.0
-        puts "#{x}^2 - #{d}*#{y.to_i}^2 = 1"
-        if x > largest_x
-          largest_x, largest_d = x, d
-        end
-        throw(:solution_found)
+  loop.with_index(2) do |_, x|
+    y = Math.sqrt( (x**2 - 1) / d.to_f )
+    if y % 1 == 0.0
+      puts "#{x}^2 - #{d}*#{y.to_i}^2 = 1"
+      if x > largest_x
+        largest_x, largest_d = x, d
       end
-
-      # loop.with_index(1) do |_, y|
-      #   res = x**2 - d * y**2
-      #
-      #   if res == 1
-      #     puts "#{x}^2 - #{d}*#{y}^2 = 1"
-      #     if x > largest_x
-      #       largest_x, largest_d = x, d
-      #     end
-      #     throw(:solution_found)
-      #   end
-      #
-      #   break if res < 1
-      # end
+      break
     end
   end
 end
