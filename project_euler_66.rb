1db26e333bbb0b052a570a00b2234524e7b8b0b3
Rustam Gasanov <rustamagasanov@gmail.com>
2015-06-12 14:12:36 +0300

    [^] slow pe66 implementation

--- project_euler_66.rb
+++ project_euler_66.rb
@@ -1,4 +1,5 @@
-(2..7).each do |d|
+largest_x, largest_d = 0, 0
+(2..1000).each do |d|
   next if Math.sqrt(d) % 1 == 0.0
   catch(:solution_found) do
     loop.with_index(1) do |_, x|
@@ -7,6 +8,9 @@
 
         if res == 1
           puts "#{x}^2 - #{d}*#{y}^2 = 1"
+          if x > largest_x
+            largest_x, largest_d = x, d
+          end
           throw(:solution_found)
         end
 
@@ -15,3 +19,5 @@
     end
   end
 end
+
+puts "largest x = #{largest_x}, largest d = #{largest_d}"
