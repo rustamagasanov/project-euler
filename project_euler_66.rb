367be1301fcb7debca954379b519be664f8428ee
Rustam Gasanov <rustamagasanov@gmail.com>
2015-06-14 22:59:44 +0300

    [^] started chakravala method implementation

--- project_euler_66.rb
+++ project_euler_66.rb
@@ -23,18 +23,47 @@
 # https://en.wikipedia.org/wiki/Pell%27s_equation
 # https://en.wikipedia.org/wiki/Chakravala_method
 # http://cs.annauniv.edu/insight/Reading/algebra/indet/chakra.htm
+# http://cs.annauniv.edu/insight/Reading/algebra/indet/chakraex.htm
 
-largest_x, largest_d = 0, 0
-(2..1_000).each do |d|
-  next if Math.sqrt(d) % 1 == 0.0
-  loop.with_index(2) do |_, x|
-    y = Math.sqrt( (x**2 - 1) / d.to_f )
-    if y % 1 == 0.0
-      puts "#{x}^2 - #{d}*#{y.to_i}^2 = 1"
-      largest_x, largest_d = x, d if x > largest_x
-      break
+# largest_x, largest_d = 0, 0
+# (2..1_000).each do |d|
+#   next if Math.sqrt(d) % 1 == 0.0
+#   loop.with_index(2) do |_, x|
+#     y = Math.sqrt( (x**2 - 1) / d.to_f )
+#     if y % 1 == 0.0
+#       puts "#{x}^2 - #{d}*#{y.to_i}^2 = 1"
+#       largest_x, largest_d = x, d if x > largest_x
+#       break
+#     end
+#   end
+# end
+#
+# puts "largest x = #{largest_x}, largest d = #{largest_d}"
+
+r = 0
+p = 1
+k = 1
+x = 1
+y = 0
+
+d = 61
+
+loop do
+  r += 1
+
+  new_p = nil
+  best_new_p_candidate = nil
+
+  loop.with_index(1) do |_, i|
+    new_p = i
+    if (p + new_p) % k == 0
+
+      # break
+    else
+      next
     end
   end
-end
 
-puts "largest x = #{largest_x}, largest d = #{largest_d}"
+
+  break if k == 1
+end
