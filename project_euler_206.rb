03b1da013ee21bf374d026ed02f99b5808cd37d4
Rustam Gasanov <rustamagasanov@gmail.com>
2015-07-27 00:50:20 +0200

    Added second approach

--- project_euler_206.rb
+++ project_euler_206.rb
@@ -3,17 +3,15 @@
 # Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 # where each “_” is a single digit.
 
-# approach 1
-(0..999_999_999).each do |i|
-  p i
-  s = i.to_s.rjust(9, '0')
-  num = "1#{s[0]}2#{s[1]}3#{s[2]}4#{s[3]}5#{s[4]}6#{s[5]}7#{s[6]}8#{s[7]}9#{s[8]}0"
-  if Math.sqrt(num.to_i) % 1 == 0.0
-    puts num
-    break
+min = 1020304050607080900
+max = 1929394959697989990
+
+loop.with_index(Math.sqrt(min).to_i) do |_, i|
+  current_try = i * i
+  p current_try
+  if current_try.to_s =~ /1\d2\d3\d4\d5\d6\d7\d8\d9\d0/
+    puts "#{i} * #{i} = #{current_try}"
   end
+  break if current_try > max
 end
 
-# approach 2
-# get squares above 1_0_0_0_0_0_0_0_0_0
-#             below 1_2_3_4_5_6_7_8_9_1
