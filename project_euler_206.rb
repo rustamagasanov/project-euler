b6bed7d3f9235a5674f292975f7ad249e53e14d4
Rustam Gasanov <rustamagasanov@gmail.com>
2015-07-28 19:18:59 +0200

    Optimised pe206

--- project_euler_206.rb
+++ project_euler_206.rb
@@ -7,10 +7,13 @@ min = 1020304050607080900
 max = 1929394959697989990
 
 loop.with_index(Math.sqrt(min).to_i) do |_, i|
+  # OPTIMISATION 1: i should be ...0 to match ...0 square
+  next if i % 10 != 0
   current_try = i * i
-  p current_try
+  puts "#{i} * #{i} = #{current_try}"
   if current_try.to_s =~ /1\d2\d3\d4\d5\d6\d7\d8\d9\d0/
     puts "#{i} * #{i} = #{current_try}"
+    break
   end
   break if current_try > max
 end
