129b31449d98cfdb67876e354bb49b3f37fb8d59
Rustam Gasanov <rustamagasanov@gmail.com>
2015-08-05 23:18:07 +0200

    Added pe78 description

---
+++ project_euler_78.rb
@@ -0,0 +1,13 @@
+# Coin partitions
+# Problem 78
+# Published on 10 September 2004 at 06:00 pm [Server Time]
+# Let p(n) represent the number of different ways in which n coins can be separated into piles. For example, five coins can be separated into piles in exactly seven different ways, so p(5)=7.
+#
+# OOOOO
+# OOOO   O
+# OOO   OO
+# OOO   O   O
+# OO   OO   O
+# OO   O   O   O
+# O   O   O   O   O
+# Find the least value of n for which p(n) is divisible by one million.
