d67d1f3eb38939a50fbaed577491ab1f76bf21d6
Rustam Gasanov <rustamagasanov@gmail.com>
2015-08-06 19:59:49 -0400

    First attempt for pe78

--- project_euler_78.rb
+++ project_euler_78.rb
@@ -11,3 +11,16 @@
 # OO   O   O   O
 # O   O   O   O   O
 # Find the least value of n for which p(n) is divisible by one million.
+
+d = [1]
+max = 60_000
+
+# loop.with_index(1) do |_, i|
+(1..max).each do |i|
+  (i..max).each do |j|
+    d[j] = 0 if d[j].nil?
+    d[j] += d[j - i]
+  end
+end
+res = d.find_index { |val| val % 1_000_000 == 0 }
+puts res if !res.nil?
