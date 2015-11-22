c07e0df0b57665c2a446013ae7aeb5a958262691
Rustam Gasanov <rustamagasanov@gmail.com>
2015-11-21 18:10:02 -0600

    Fixed pe113 desc

--- project_euler_113.rb
+++ project_euler_113.rb
@@ -6,6 +6,7 @@
 #
 # We shall call a positive integer that is neither increasing nor decreasing a "bouncy" number; for example, 155349.
 #
-# As n increases, the proportion of bouncy numbers below n increases such that there are only 12951 numbers below one-million that are not bouncy and only 277032 non-bouncy numbers below 1010.
+# As n increases, the proportion of bouncy numbers below n increases such that there are only 12951 numbers below one-million that are not bouncy and only 277032 non-bouncy numbers below 10^10.
 #
-# How many numbers below a googol (10100) are not bouncy?
+# How many numbers below a googol (10^100) are not bouncy?
+
