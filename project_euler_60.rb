81fd8953dd6d31a6cc74987edb607fcf48b7537f
Rustam Gasanov <rustamagasanov@gmail.com>
2015-12-07 23:34:07 +0100

    Added sieve and prime checker

--- project_euler_60.rb
+++ project_euler_60.rb
@@ -3,3 +3,35 @@
 # The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 #
 # Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+
+class EratosthenesSieve
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def get_primes
+    res = (0..limit).to_a
+    (2..Math.sqrt(limit)).each do |i|
+      if res[i].nil?
+        next
+      else
+        (res[i] * 2..limit).step(res[i]) { |j| res[j] = nil }
+      end
+    end
+    res[2..-1].compact
+  end
+end
+
+class PrimeChecker
+  def self.prime?(n)
+    return false if n < 2
+    (2..Math.sqrt(n)).each do |i|
+      return false if n % i == 0
+    end
+    true
+  end
+end
+
+primes = EratosthenesSieve.new(999_999).get_primes
