d657c237f4d174ff77477c05aa81ca3fe477e8a2
Rustam Gasanov <rustamagasanov@gmail.com>
2016-03-29 19:24:19 -0500

    Recursive algorithm tryout

--- project_euler_61.rb
+++ project_euler_61.rb
@@ -64,36 +64,57 @@ numbers[:hexagonals]  = polygonal_numbers('hexagonal')
 numbers[:heptagonals] = polygonal_numbers('heptagonal')
 numbers[:octagonals]  = polygonal_numbers('octagonal')
 
-catch(:done) do
-  [:triangles, :squares, :pentagonals, :hexagonals, :heptagonals, :octagonals].permutation.each do |permutation|
-    numbers[permutation[0]].each do |n0|
-      numbers[permutation[1]].each do |n1|
-        if n0.to_s[2..3] == n1.to_s[0..1]
-          numbers[permutation[2]].each do |n2|
-            if n1.to_s[2..3] == n2.to_s[0..1]
-              numbers[permutation[3]].each do |n3|
-                if n2.to_s[2..3] == n3.to_s[0..1]
-                  numbers[permutation[4]].each do |n4|
-                    if n3.to_s[2..3] == n4.to_s[0..1]
-                      numbers[permutation[5]].each do |n5|
-                        if n4.to_s[2..3] == n5.to_s[0..1]
-                          if n5.to_s[2..3] == n0.to_s[0..1]
-                            p permutation
-                            puts "#{n0} #{n1} #{n2} #{n3} #{n4} #{n5}"
-                            puts [n0, n1, n2, n3, n4, n5].reduce(:+)
-                            throw(:done)
-                          end
-                        end
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
+def calculate(groups, remaining_groups, numbers, result = [])
+  p result
+  return false if remaining_groups.size < 2
+  numbers[remaining_groups[0]].each do |n0|
+    numbers[remaining_groups[1]].each do |n1|
+      if remaining_groups.size == 2 && n0.to_s[2..3] == n1.to_s[0..1] && n1.to_s[2..3] == result[0]
+        result + [n1]
+      elsif n0.to_s[2..3] == n1.to_s[0..1]
+        res = remaining_groups == groups ? [n0, n1] : result + [n1]
+        calculate(groups, remaining_groups[1..-1], numbers, res)
+      else
+        false
       end
     end
   end
 end
 
+[:triangles, :squares, :pentagonals, :hexagonals, :heptagonals, :octagonals].permutation.each do |permutation|
+  p calculate(permutation, permutation, numbers)
+end
+
+# catch(:done) do
+#   [:triangles, :squares, :pentagonals, :hexagonals, :heptagonals, :octagonals].permutation.each do |permutation|
+#     numbers[permutation[0]].each do |n0|
+#       numbers[permutation[1]].each do |n1|
+#         if n0.to_s[2..3] == n1.to_s[0..1]
+#           numbers[permutation[2]].each do |n2|
+#             if n1.to_s[2..3] == n2.to_s[0..1]
+#               numbers[permutation[3]].each do |n3|
+#                 if n2.to_s[2..3] == n3.to_s[0..1]
+#                   numbers[permutation[4]].each do |n4|
+#                     if n3.to_s[2..3] == n4.to_s[0..1]
+#                       numbers[permutation[5]].each do |n5|
+#                         if n4.to_s[2..3] == n5.to_s[0..1]
+#                           if n5.to_s[2..3] == n0.to_s[0..1]
+#                             p permutation
+#                             puts "#{n0} #{n1} #{n2} #{n3} #{n4} #{n5}"
+#                             puts [n0, n1, n2, n3, n4, n5].reduce(:+)
+#                             throw(:done)
+#                           end
+#                         end
+#                       end
+#                     end
+#                   end
+#                 end
+#               end
+#             end
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
+#
