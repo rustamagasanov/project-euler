436b408c6e3cc6251ee658a1abf29664d123833a
Rustam Gasanov <rustamagasanov@gmail.com>
2016-04-04 18:27:38 -0400

    Completed vertical pieces widening algo

--- project_euler_85.rb
+++ project_euler_85.rb
@@ -4,14 +4,19 @@
 #
 # Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
 
-a = 8
-b = 2
+a = 3
+b = 3
 
 res = []
 
 # horizontal piece widening (1x1, 2x1, 3x1 etc)
 (1..a).each do |i|
-  res << (a - i + 1) * 2
+  res << (a - i + 1) * b
+end
+
+# vertical piece widening (1x1 skipped, 1x2, 1x3 etc)
+(2..b).each do |i|
+  res << (b - i + 1) * a
 end
 
 p res
